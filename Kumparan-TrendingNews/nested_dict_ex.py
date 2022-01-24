1e4e3ad3a3c053c3355fa676da7c949f713e111f
christianyurianja <chris37fx@gmail.com>
2022-01-24 19:52:54 +0700

    Separate into 2 function: 1. Data extraction that save the Trending news data into nested dictionary. 2. Show data that print the value in the nested dictionary.

---
+++ Kumparan-TrendingNews/nested_dict_ex.py
@@ -0,0 +1,10 @@
+people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
+          2: {'name': 'Marie', 'age': '22', 'sex': 'Female'}}
+
+print('\n', people)
+
+# for id, info in people.items():
+#     print("\nPerson ID:", id)
+#
+#     for key in info:
+#         print(key + ':', info[key])
